0b243094b4a98a1cc6eb2f7b7cbec4f60d7c14a6
jhonmunthe <jhonmunthe25@gmail.com>
2024-10-19 18:19:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 // script.js
 
-// Fungsi untuk memunculkan menu utama dan menyembunyikan form login
+
 function login() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    // Simulasi pengecekan username dan password
+  
     if (username === '111' && password === '111') {
-        // Sembunyikan form login
+
         document.getElementById('login-container').style.display = 'none';
 
-        // Tampilkan konten utama
+
         document.querySelector('header').style.display = 'block';
         document.querySelector('.hero').style.display = 'flex';
         document.getElementById('about').style.display = 'block';
         document.querySelector('footer').style.display = 'block';
 
-        return false; // Untuk mencegah form submit ke server
+        return false;
     } else {
         alert('Username atau password salah');
-        return false; // Mencegah form tetap disubmit jika salah
+        return false;
     }
 }
